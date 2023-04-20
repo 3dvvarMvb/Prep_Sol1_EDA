7c5a6ef268b400c0b5beeba3799a192cbb836202
3dvvarMvb <eduarmaxi.2003@gmail.com>
2023-04-20 09:18:36 -0400

    HR:9:18_20-04-23

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -2,7 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Ejercicio1 {
-    public static void main(String[] args){
+    public static void Ordenamiento(int[] A, int n){
+        if(n==1){
+            if(A[n]>A[n-1]){
+                int temp=A[n];
+                A[n]=A[n-1];
+                A[n-1]=temp;
+            }
+            return;
+        }
+        if(n%2!=0){
+            if(A[n]>A[n-1]){
+                int temp=A[n];
+                A[n]=A[n-1];
+                A[n-1]=temp;
+            }
+        }
+        else{
+            if(A[n]<A[n-1]){
+                int temp=A[n];
+                A[n]=A[n-1];
+                A[n-1]=temp;
+            }
+        }
+        Ordenamiento(A,(n-1));
+    }
+    public static void PrintArray(int[] A,int indice){
+        if(indice==A.length){
+            return;
+        }
+        System.out.print(A[indice]+" ");
+        PrintArray(A,indice+1);
+    }
 
+    public static void main(String[] args){
+        int [] Array=new int[]{12, 4, 6, 4, 2, 22, 30, 25};
+        int N= Array.length-1;
+        Ordenamiento(Array,N);
+        PrintArray(Array,0);
     }
 }
---
+++ src/Ejercicio2.java
@@ -0,0 +1,7 @@
+import java.io.*;
+import java.util.*;
+public class Ejercicio2 {
+    public static void main(String[] args){
+
+    }
+}
